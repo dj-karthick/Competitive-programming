e768d63620303690a76160f081f9be5f77d57470
dj-karthick <karthickallancherry@gmail.com>
2025-02-26 19:16:01 +0530

    Repractising the patterns for better understanding

--- pattern2.cpp
+++ pattern2.cpp
@@ -9,9 +9,39 @@
 #include <limits>           // For numeric limits (e.g., infinity)
 #include <cmath>            // For mathematical operations
 
+
 using namespace std;
 
+void pattern1(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void pattern2(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void pattern3(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j)
+    }
+}
 int main() {
-    cout << "Hey buddy how are you";
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++){
+        int n;
+        cin >> n;
+        pattern3(n);
+    }
     return 0;
-} 
+}
